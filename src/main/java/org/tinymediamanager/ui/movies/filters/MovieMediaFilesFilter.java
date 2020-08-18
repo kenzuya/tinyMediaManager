70bc9b42c75c760c94e6c21351d3d85f84a89e42
Manuel Laggner <manuel.laggner@gmail.com>
2020-08-18 12:37:13 +0200

    fix: refactored media file type filter

--- src/main/java/org/tinymediamanager/ui/movies/filters/MovieMediaFilesFilter.java
+++ src/main/java/org/tinymediamanager/ui/movies/filters/MovieMediaFilesFilter.java
@@ -1,38 +1,48 @@
 package org.tinymediamanager.ui.movies.filters;
 
-import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 import javax.swing.JLabel;
 
-import org.tinymediamanager.core.Constants;
 import org.tinymediamanager.core.MediaFileType;
 import org.tinymediamanager.core.entities.MediaFile;
-import org.tinymediamanager.core.movie.MovieList;
 import org.tinymediamanager.core.movie.entities.Movie;
 import org.tinymediamanager.ui.components.TmmLabel;
 
-public class MovieMediaFilesFilter extends AbstractCheckComboBoxMovieUIFilter<MediaFileType> {
-
-  private MovieList movieList = MovieList.getInstance();
+public class MovieMediaFilesFilter extends AbstractCheckComboBoxMovieUIFilter<MovieMediaFilesFilter.MediaFileTypeContainer> {
 
   public MovieMediaFilesFilter() {
     super();
-    buildAndInstallCertificationArray();
-    PropertyChangeListener propertyChangeListener = evt -> buildAndInstallCertificationArray();
-    movieList.addPropertyChangeListener(Constants.MEDIA_FILES, propertyChangeListener);
+
+    List<MediaFileTypeContainer> mediaFileTypeList = new ArrayList<>();
+
+    for (MediaFileType type : MediaFileType.values()) {
+      switch (type) {
+        // explicit exclude
+        case VIDEO_EXTRA:
+        case GRAPHIC:
+        case DOUBLE_EXT:
+          break;
+
+        default:
+          mediaFileTypeList.add(new MediaFileTypeContainer(type));
+          break;
+
+      }
+    }
+    setValues(mediaFileTypeList);
   }
 
   @Override
-  protected String parseTypeToString(MediaFileType type) throws Exception {
-    return type.name();
+  protected String parseTypeToString(MediaFileTypeContainer container) throws Exception {
+    return container.type.name();
   }
 
   @Override
-  protected MediaFileType parseStringToType(String string) throws Exception {
-    return MediaFileType.valueOf(string);
+  protected MediaFileTypeContainer parseStringToType(String string) throws Exception {
+    return new MediaFileTypeContainer(MediaFileType.valueOf(string));
   }
 
   @Override
@@ -50,7 +60,11 @@ public class MovieMediaFilesFilter extends AbstractCheckComboBoxMovieUIFilter<Me
 
     boolean isValid = false;
 
-    List<MediaFileType> selectedItems = checkComboBox.getSelectedItems();
+    List<MediaFileType> selectedItems = new ArrayList<>();
+    for (MediaFileTypeContainer container : checkComboBox.getSelectedItems()) {
+      selectedItems.add(container.type);
+    }
+
     for (MediaFile mf : movie.getMediaFiles()) {
       if (selectedItems.contains(mf.getType())) {
         isValid = true;
@@ -61,10 +75,21 @@ public class MovieMediaFilesFilter extends AbstractCheckComboBoxMovieUIFilter<Me
     return isValid;
   }
 
-  private void buildAndInstallCertificationArray() {
-    List<MediaFileType> mediaFileTypeList = new ArrayList<>();
-    mediaFileTypeList.addAll(Arrays.asList(MediaFileType.values()));
-    setValues(mediaFileTypeList);
-  }
+  public static class MediaFileTypeContainer {
+    private final MediaFileType type;
 
+    public MediaFileTypeContainer(MediaFileType type) {
+      this.type = type;
+    }
+
+    @Override
+    public String toString() {
+      try {
+        return BUNDLE.getString("mediafiletype." + type.name().toLowerCase(Locale.ROOT));
+      }
+      catch (Exception e) {
+        return type.toString();
+      }
+    }
+  }
 }
