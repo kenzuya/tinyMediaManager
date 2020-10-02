ef84e3abd986f6d6607a765b2b26492751373a0b
Manuel Laggner <manuel.laggner@gmail.com>
2020-10-02 09:58:01 +0200

    fix: also respect current file names in the file name filter
    fixes #1007

--- src/main/java/org/tinymediamanager/ui/movies/filters/MovieFilenameFilter.java
+++ src/main/java/org/tinymediamanager/ui/movies/filters/MovieFilenameFilter.java
@@ -5,6 +5,8 @@ import java.util.regex.Matcher;
 import javax.swing.JLabel;
 
 import org.apache.commons.lang3.StringUtils;
+import org.tinymediamanager.core.MediaFileType;
+import org.tinymediamanager.core.entities.MediaFile;
 import org.tinymediamanager.core.movie.entities.Movie;
 import org.tinymediamanager.scraper.util.StrgUtils;
 import org.tinymediamanager.ui.components.TmmLabel;
@@ -12,7 +14,7 @@ import org.tinymediamanager.ui.components.TmmLabel;
 public class MovieFilenameFilter extends AbstractTextMovieUIFilter {
   @Override
   protected JLabel createLabel() {
-    return new TmmLabel(BUNDLE.getString("metatag.originalfile"));
+    return new TmmLabel(BUNDLE.getString("metatag.filename"));
   }
 
   @Override
@@ -27,10 +29,19 @@ public class MovieFilenameFilter extends AbstractTextMovieUIFilter {
     }
 
     try {
-      // Filename
+      for (MediaFile mediaFile : movie.getMediaFiles(MediaFileType.VIDEO)) {
+        Matcher matcher = filterPattern.matcher(StrgUtils.normalizeString(mediaFile.getFilename()));
+        if (matcher.find()) {
+          return true;
+        }
+      }
+
+      // also have a look at the original filename
       if (StringUtils.isNotEmpty(movie.getOriginalFilename())) {
         Matcher matcher = filterPattern.matcher(StrgUtils.normalizeString(movie.getOriginalFilename()));
-        return matcher.find();
+        if (matcher.find()) {
+          return true;
+        }
       }
     }
     catch (Exception e) {
