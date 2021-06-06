9d4fefe9dee7ec4042ab5ba56588c0fe0e38954a
Manuel Laggner <manuel.laggner@gmail.com>
2021-06-06 20:22:22 +0200

    fix: reading existing NFO files from disc folders

--- src/test/java/org/tinymediamanager/core/movie/tasks/MovieUpdateDatasourceTaskTest.java
+++ src/test/java/org/tinymediamanager/core/movie/tasks/MovieUpdateDatasourceTaskTest.java
@@ -1,4 +1,20 @@
-package org.tinymediamanager.core.movie;
+/*
+ * Copyright 2012 - 2021 Manuel Laggner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tinymediamanager.core.movie.tasks;
 
 import java.io.File;
 import java.nio.file.Paths;
@@ -13,8 +29,9 @@ import org.tinymediamanager.BasicTest;
 import org.tinymediamanager.core.Settings;
 import org.tinymediamanager.core.TmmModuleManager;
 import org.tinymediamanager.core.Utils;
+import org.tinymediamanager.core.movie.MovieList;
+import org.tinymediamanager.core.movie.MovieModuleManager;
 import org.tinymediamanager.core.movie.entities.Movie;
-import org.tinymediamanager.core.movie.tasks.MovieUpdateDatasourceTask;
 
 /**
  * This class cannot run, since Settings() is STATIC<br>
