9bf96525275e33f7752573fb370665df057b02e8
Manuel Laggner <manuel.laggner@gmail.com>
2021-06-23 20:42:16 +0200

    fix: (Linux) pre-set installed FFmpeg on first run

--- src/main/java/org/tinymediamanager/core/Settings.java
+++ src/main/java/org/tinymediamanager/core/Settings.java
@@ -15,6 +15,9 @@
  */
 package org.tinymediamanager.core;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -25,6 +28,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.commons.text.StringEscapeUtils;
 import org.jdesktop.observablecollections.ObservableCollections;
 import org.slf4j.Logger;
@@ -235,7 +239,25 @@ public final class Settings extends AbstractSettings {
 
     setProxyFromSystem();
 
+    // set FFmpeg from the system
+    findFfmpegPath();
+
     saveSettings();
+  }
+
+  private void findFfmpegPath() {
+    if (SystemUtils.IS_OS_LINUX) {
+      Path ffmpeg = Paths.get("/usr/bin/ffmpeg");
+      if (Files.exists(ffmpeg) && Files.isExecutable(ffmpeg)) {
+        mediaFramework = ffmpeg.toString();
+      }
+      else {
+        ffmpeg = Paths.get("/usr/local/bin/ffmpeg");
+        if (Files.exists(ffmpeg) && Files.isExecutable(ffmpeg)) {
+          mediaFramework = ffmpeg.toString();
+        }
+      }
+    }
   }
 
   /**
