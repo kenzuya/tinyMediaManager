3c7551aa5e18848ab507b8cd06db846efe8f834e
Manuel Laggner <manuel.laggner@gmail.com>
2021-09-08 19:58:52 +0200

    fix: calculation of minimum size for restoring window sizes/positions
    use JDialog.getMinimumSize() rather than JDialog.getWidth()/.getHeight()
    fixes #1485

--- src/main/java/org/tinymediamanager/ui/TmmUILayoutStore.java
+++ src/main/java/org/tinymediamanager/ui/TmmUILayoutStore.java
@@ -20,6 +20,7 @@ import static java.awt.Frame.MAXIMIZED_BOTH;
 import java.awt.AWTEvent;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
@@ -183,13 +184,14 @@ public class TmmUILayoutStore implements AWTEventListener {
 
     if (!dialog.getName().contains("dialog")) {
       Rectangle rect = getWindowBounds(dialog.getName());
+      Dimension minimumSize = dialog.getMinimumSize();
 
       // re-check if the stored window size is "big" enough (the "default" size has already been set with .pack())
-      if (rect.width < dialog.getWidth()) {
-        rect.width = dialog.getWidth();
+      if (rect.width < minimumSize.width) {
+        rect.width = minimumSize.width;
       }
-      if (rect.height < dialog.getHeight()) {
-        rect.height = dialog.getHeight();
+      if (rect.height < minimumSize.height) {
+        rect.height = minimumSize.height;
       }
 
       if (rect.width > 0 && getVirtualBounds().contains(rect)) {
