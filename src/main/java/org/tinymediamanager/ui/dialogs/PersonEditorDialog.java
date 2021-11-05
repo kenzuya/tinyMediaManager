1ed38972e6439cc5e2ff7ab9d0697dfc1b39830d
Manuel Laggner <manuel.laggner@gmail.com>
2021-11-05 14:48:32 +0100

    feat: enabled editing of person IDs
    fixes #1559

--- src/main/java/org/tinymediamanager/ui/dialogs/PersonEditorDialog.java
+++ src/main/java/org/tinymediamanager/ui/dialogs/PersonEditorDialog.java
@@ -18,18 +18,32 @@ package org.tinymediamanager.ui.dialogs;
 
 import java.awt.Dialog;
 import java.awt.Window;
+import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import org.apache.commons.lang3.StringUtils;
 import org.tinymediamanager.core.TmmResourceBundle;
 import org.tinymediamanager.core.entities.Person;
+import org.tinymediamanager.scraper.ScraperType;
 import org.tinymediamanager.ui.IconManager;
+import org.tinymediamanager.ui.components.MediaIdTable;
+import org.tinymediamanager.ui.components.MediaIdTable.MediaId;
+import org.tinymediamanager.ui.components.SquareIconButton;
+import org.tinymediamanager.ui.components.TmmLabel;
+import org.tinymediamanager.ui.components.table.TmmTable;
 
+import ca.odell.glazedlists.EventList;
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -38,17 +52,21 @@ import net.miginfocom.swing.MigLayout;
  * @author Manuel Laggner
  */
 public class PersonEditorDialog extends TmmDialog {
-  private static final long serialVersionUID = 535326891112742179L;
-  private final Person      personToEdit;
+  private static final long        serialVersionUID = 535326891112742179L;
 
-  private JTextField        tfName;
-  private JTextField        tfRole;
-  private JTextField        tfImageUrl;
-  private JTextField        tfProfileUrl;
+  private final Person             personToEdit;
+  private final EventList<MediaId> ids;
+
+  private JTextField               tfName;
+  private JTextField               tfRole;
+  private JTextField               tfImageUrl;
+  private JTextField               tfProfileUrl;
+  private TmmTable                 tableIds;
 
   public PersonEditorDialog(Window owner, String title, Person person) {
     super(owner, title, "personEditor");
     personToEdit = person;
+    this.ids = MediaIdTable.convertIdMapToEventList(personToEdit.getIds());
 
     initComponents();
 
@@ -66,7 +84,7 @@ public class PersonEditorDialog extends TmmDialog {
     {
       JPanel panelContent = new JPanel();
       getContentPane().add(panelContent);
-      panelContent.setLayout(new MigLayout("", "[][300lp:n,grow][]", "[][][][]"));
+      panelContent.setLayout(new MigLayout("", "[][500lp:n,grow][]", "[][][][][][][][20lp]"));
       {
         JLabel lblNameT = new JLabel(TmmResourceBundle.getString("metatag.name"));
         panelContent.add(lblNameT, "cell 0 0,alignx trailing");
@@ -116,6 +134,22 @@ public class PersonEditorDialog extends TmmDialog {
         panelContent.add(tfProfileUrl, "cell 1 3,growx");
         tfProfileUrl.setColumns(10);
       }
+      {
+        JLabel lblIds = new TmmLabel(TmmResourceBundle.getString("metatag.ids"));
+        panelContent.add(lblIds, "cell 0 4,alignx trailing");
+
+        JScrollPane scrollPaneIds = new JScrollPane();
+        panelContent.add(scrollPaneIds, "cell 1 4 1 4,grow");
+
+        tableIds = new MediaIdTable(ids);
+        tableIds.configureScrollPane(scrollPaneIds);
+
+        JButton btnAddId = new SquareIconButton(new AddIdAction());
+        panelContent.add(btnAddId, "cell 0 5,alignx right,aligny top");
+      }
+
+      JButton btnRemoveId = new SquareIconButton(new RemoveIdAction());
+      panelContent.add(btnRemoveId, "cell 0 6,alignx right,aligny top");
     }
     {
       JButton btnCancel = new JButton(TmmResourceBundle.getString("Button.cancel"));
@@ -128,9 +162,81 @@ public class PersonEditorDialog extends TmmDialog {
         personToEdit.setRole(tfRole.getText());
         personToEdit.setThumbUrl(tfImageUrl.getText());
         personToEdit.setProfileUrl(tfProfileUrl.getText());
+
+        // sync of media ids
+        // first round -> add existing ids
+        for (MediaId id : ids) {
+          // only process non empty ids
+          // changed; if empty/0/null value gets set, it is removed in setter ;)
+          // if (StringUtils.isAnyBlank(id.key, id.value)) {
+          // continue;
+          // }
+          // first try to cast it into an Integer
+          try {
+            Integer value = Integer.parseInt(id.value);
+            // cool, it is an Integer
+            personToEdit.setId(id.key, value);
+          }
+          catch (NumberFormatException ex) {
+            // okay, we set it as a String
+            personToEdit.setId(id.key, id.value);
+          }
+        }
+        // second round -> remove deleted ids
+        List<String> removeIds = new ArrayList<>();
+        for (Entry<String, Object> entry : personToEdit.getIds().entrySet()) {
+          MediaId id = new MediaId(entry.getKey());
+          if (!ids.contains(id)) {
+            removeIds.add(entry.getKey());
+          }
+        }
+        for (String id : removeIds) {
+          // set a null value causes to fire the right events
+          personToEdit.setId(id, null);
+        }
+
         setVisible(false);
       });
       addDefaultButton(btnOk);
+    }
+  }
+
+  private class AddIdAction extends AbstractAction {
+    private static final long serialVersionUID = 2903255414553349267L;
+
+    public AddIdAction() {
+      putValue(SHORT_DESCRIPTION, TmmResourceBundle.getString("id.add"));
+      putValue(SMALL_ICON, IconManager.ADD_INV);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      MediaId mediaId = new MediaId();
+      IdEditorDialog dialog = new IdEditorDialog(SwingUtilities.getWindowAncestor(tableIds), TmmResourceBundle.getString("id.add"), mediaId,
+          ScraperType.MOVIE);
+      dialog.setVisible(true);
+
+      if (StringUtils.isNoneBlank(mediaId.key, mediaId.value)) {
+        ids.add(mediaId);
+      }
+    }
+  }
+
+  private class RemoveIdAction extends AbstractAction {
+    private static final long serialVersionUID = -7079826950827356996L;
+
+    public RemoveIdAction() {
+      putValue(SHORT_DESCRIPTION, TmmResourceBundle.getString("id.remove"));
+      putValue(SMALL_ICON, IconManager.REMOVE_INV);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      int row = tableIds.getSelectedRow();
+      if (row > -1) {
+        row = tableIds.convertRowIndexToModel(row);
+        ids.remove(row);
+      }
     }
   }
 }
