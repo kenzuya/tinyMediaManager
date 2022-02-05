81826657d54cf5e523ff6cd6b3a24254222e0f38
Manuel Laggner <manuel.laggner@gmail.com>
2022-02-05 09:50:46 +0100

    fix: moved the scroll buttons in the uds submenu to the right place

--- src/main/java/org/tinymediamanager/ui/movies/MovieUIModule.java
+++ src/main/java/org/tinymediamanager/ui/movies/MovieUIModule.java
@@ -193,7 +193,7 @@ public class MovieUIModule extends AbstractTmmUIModule {
 
     JMenu datasourcesMenu = new JMenu(TmmResourceBundle.getString("metatag.datasource"));
     datasourcesMenu.setIcon(IconManager.MENU);
-    MenuScroller.setScrollerFor(datasourcesMenu, 20, 25, 2, 5);
+    MenuScroller.setScrollerFor(datasourcesMenu, 20, 25, 0, 0);
     datasourcesMenu.getPopupMenu().addPopupMenuListener(new PopupMenuListener() {
       @Override
       public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
