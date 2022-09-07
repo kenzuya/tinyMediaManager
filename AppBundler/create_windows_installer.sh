e8e71c223ac6275fc115b5fa7e4af6a2008d9912
Myron Boyle <myron0815@gmx.net>
2022-09-07 18:54:59 +0200

    🐛 fix my SH attempts
    [ci skip]

--- AppBundler/create_windows_installer.sh
+++ AppBundler/create_windows_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 VER=""
-if [ ! -z "$1" ] then
+if [ -n "$1" ] ; then
     VER="/DMyAppVersion=$1"
 fi
 
@@ -9,7 +9,7 @@ mkdir windows_installer
 cd windows_installer
 unzip ../target/tinyMediaManager*-windows-*.zip
 cp ../AppBundler/installer.iss .
-iscc installer.iss $VER
+iscc installer.iss "$VER"
 cp Output/tinyMediaManagerSetup.exe ../dist/
 cd ..
 rm -rf windows_installer
