761f858af84bbfb4cd80f59c59b29813c2b02da1
Myron Boyle <myron0815@gmx.net>
2022-09-08 14:26:02 +0200

    jtdh

--- AppBundler/create_windows_installer.sh
+++ AppBundler/create_windows_installer.sh
@@ -9,7 +9,6 @@ cd tinyMediaManager
 cp ../../AppBundler/installer.iss .
 
 ls -l 
-ls -l ../target/
 
 VERSION=$(grep 'version' version | cut -d'=' -f2)
 TIMESTAMP=$(grep 'timestamp' version | cut -d'=' -f2)
@@ -18,6 +17,7 @@ REALVERSION=$(echo $VERSION | sed "s/SNAPSHOT/$TIMESTAMP/g")
 iscc installer.iss "/DMyAppVersion=$REALVERSION"
 
 cp Output/tinyMediaManagerSetup.exe ../../target/
+ls -l ../../target/
 cd ..
 cd ..
 rm -rf windows_installer
