7e159b4c8457f958caf21c14f02c59a5456ad22c
Myron Boyle <myron0815@gmx.net>
2022-09-08 14:04:15 +0200

    sdf

--- AppBundler/create_windows_installer.sh
+++ AppBundler/create_windows_installer.sh
@@ -6,6 +6,8 @@ cd windows_installer
 unzip ../target/tinyMediaManager*-windows-*.zip tinyMediaManager/*
 cp ../AppBundler/installer.iss .
 
+ls -l windows_installer
+
 VERSION=$(grep 'version' tinyMediaManager/version | cut -d'=' -f2)
 TIMESTAMP=$(grep 'timestamp' tinyMediaManager/version | cut -d'=' -f2)
 REALVERSION=$(echo $VERSION | sed "s/SNAPSHOT/$TIMESTAMP/g")
