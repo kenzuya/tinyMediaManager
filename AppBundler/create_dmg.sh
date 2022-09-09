57a446fe60a017ba3bafa3d9f8de7ef35d08d4fe
Myron Boyle <myron0815@gmx.net>
2022-09-09 11:17:39 +0200

    🚧 use /dist folder again, and introduce human readable version

--- AppBundler/create_dmg.sh
+++ AppBundler/create_dmg.sh
@@ -3,11 +3,7 @@
 mkdir macos_dmg
 cd macos_dmg
 unzip -X ../target/tinyMediaManager-*macos-x86_64.zip -d tinyMediaManager
-
-VERSION=$(grep 'version' tinyMediaManager/tinyMediaManager.app/Contents/Resources/Java/version | cut -d'=' -f2)
-BUILD=$(grep 'build' tinyMediaManager/tinyMediaManager.app/Contents/Resources/Java/version | cut -d'=' -f2)
-REALVERSION=$(echo $VERSION | sed "s/SNAPSHOT/$BUILD/g")
-
+VERSION=$(grep 'human.version' tinyMediaManager/tinyMediaManager.app/Contents/Resources/Java/version | cut -d'=' -f2)
 OUTSIZE=$(du -s tinyMediaManager | cut -f1)
 dd if=/dev/zero of=temp.dmg bs=1124 count="${OUTSIZE}"
 mkfs.hfsplus -v "tinyMediaManager" temp.dmg
@@ -17,6 +13,6 @@ hfsplus temp.dmg symlink "Applications" /Applications
 hfsplus temp.dmg chmod 755 tinyMediaManager.app/Contents/MacOS/tinyMediaManager
 hfsplus temp.dmg chmod 755 tinyMediaManager.app/Contents/MacOS/JavaApplicationStub
 dmg dmg temp.dmg tinyMediaManager.dmg
-cp tinyMediaManager.dmg ../target/tinyMediaManager-$REALVERSION-macos-x86_64.dmg
+cp tinyMediaManager.dmg ../dist/tinyMediaManager-$VERSION-macos-x86_64.dmg
 cd ..
 rm -rf macos_dmg
