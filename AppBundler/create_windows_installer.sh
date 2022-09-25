09a5faa8dbc3bc88e0ca525692b99eaf3da490ea
Manuel Laggner <manuel.laggner@gmail.com>
2022-09-25 19:52:33 +0200

    🚧 code sign windows installer

--- AppBundler/create_windows_installer.sh
+++ AppBundler/create_windows_installer.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# prepare
 mkdir windows_installer
 cd windows_installer
 unzip ../dist/tmm_*_windows*.zip
 cp ../AppBundler/installer.iss .
 cp ../AppBundler/tmm.ico .
+
+# build
 iscc installer.iss
-cp Output/tinyMediaManagerSetup.exe ../dist/
+
+# sign
+echo "$CODE_SIGN_CERT" | base64 -d > code-sign-cert.p12
+osslsigncode -sign -pkcs12 "code-sign-cert.p12" -pass "$CODE_SIGN_PASS" -n "tinyMediaManager" -i https://www.tinymediamanager.org/ -in Output/tinyMediaManagerSetup.exe -out ../dist/tinyMediaManagerSetup.exe
+
+# cleanup
 cd ..
 rm -rf windows_installer
