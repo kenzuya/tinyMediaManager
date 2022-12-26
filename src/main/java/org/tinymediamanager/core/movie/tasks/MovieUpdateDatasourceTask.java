82e2e32b776de266ab6939965f96a428deb2fcc3
Manuel Laggner <manuel.laggner@gmail.com>
2022-12-26 11:15:12 +0100

    🚸 enhanced detection of movie titles when parsing the data source
    up to now, we imported "Bource Legacy, The" 1:1 in the movie title. Now the "The" will be moved to the front

--- src/main/java/org/tinymediamanager/core/movie/tasks/MovieUpdateDatasourceTask.java
+++ src/main/java/org/tinymediamanager/core/movie/tasks/MovieUpdateDatasourceTask.java
@@ -850,7 +850,7 @@ public class MovieUpdateDatasourceTask extends TmmThreadPool {
       }
 
       // get the "cleaner" name/year combo from
-      movie.setTitle(video[0]);
+      movie.setTitle(Utils.removeSortableName(video[0]));
       if (!video[1].isEmpty()) {
         try {
           movie.setYear(Integer.parseInt(video[1]));
@@ -864,7 +864,7 @@ public class MovieUpdateDatasourceTask extends TmmThreadPool {
       if (StringUtils.isNotBlank(bdmtTitle)) {
         video = ParserUtils.detectCleanTitleAndYear(bdmtTitle, MovieModuleManager.getInstance().getSettings().getBadWord());
         if (!video[0].isEmpty()) {
-          movie.setTitle(video[0]);
+          movie.setTitle(Utils.removeSortableName(video[0]));
         }
         if (!video[1].isEmpty()) {
           try {
@@ -884,7 +884,7 @@ public class MovieUpdateDatasourceTask extends TmmThreadPool {
     }
     else if (StringUtils.isBlank(movie.getTitle())) {
       // .45 for ex
-      movie.setTitle(videoName);
+      movie.setTitle(Utils.removeSortableName(videoName));
     }
 
     movie.setPath(movieDir.toAbsolutePath().toString());
