da6271a718acd22c84787c0f8f1bb195fe6fc2a2
Manuel Laggner <manuel.laggner@gmail.com>
2023-01-09 19:30:54 +0100

    ✨ fixed adding of new extrathumbs/extrafanarts
    #1996

--- src/main/java/org/tinymediamanager/core/movie/entities/Movie.java
+++ src/main/java/org/tinymediamanager/core/movie/entities/Movie.java
@@ -720,7 +720,9 @@ public class Movie extends MediaEntity implements IMediaInformation {
    */
   @JsonSetter
   public void setExtraThumbs(List<String> extraThumbs) {
-    ListUtils.mergeLists(this.extraThumbs, extraThumbs);
+    this.extraThumbs.clear();
+    this.extraThumbs.addAll(extraThumbs);
+    firePropertyChange("extraThumbs", null, this.extraThumbs);
   }
 
   /**
@@ -740,7 +742,9 @@ public class Movie extends MediaEntity implements IMediaInformation {
    */
   @JsonSetter
   public void setExtraFanarts(List<String> extraFanarts) {
-    ListUtils.mergeLists(this.extraFanarts, extraFanarts);
+    this.extraFanarts.clear();
+    this.extraFanarts.addAll(extraFanarts);
+    firePropertyChange("extraFanarts", null, this.extraFanarts);
   }
 
   /**
