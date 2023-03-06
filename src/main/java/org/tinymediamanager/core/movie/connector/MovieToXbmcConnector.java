14378acbd908c655bfe4fa69c50e82e9e8e3e517
Myron Boyle <myron0815@gmx.net>
2023-03-06 09:47:30 +0100

    🐛 [NFO] revert: lastPlayedDate in correct format
    back to date+time

--- src/main/java/org/tinymediamanager/core/movie/connector/MovieToXbmcConnector.java
+++ src/main/java/org/tinymediamanager/core/movie/connector/MovieToXbmcConnector.java
@@ -126,7 +126,7 @@ public class MovieToXbmcConnector extends MovieGenericXmlConnector {
     Element genre = getSingleElementByTag("genre");
 
     if (parser != null && parser.lastplayed != null) {
-      lastplayed.setTextContent(new SimpleDateFormat("yyyy-MM-dd").format(parser.lastplayed));
+      lastplayed.setTextContent(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(parser.lastplayed));
       root.insertBefore(lastplayed, genre);
     }
   }
