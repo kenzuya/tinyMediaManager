4f36033453891b66719b9a44ef2e72de6d687141
Manuel Laggner <manuel.laggner@gmail.com>
2023-04-17 21:02:59 +0200

    ⚡️ movie uds: check which files to cache

--- src/main/java/org/tinymediamanager/core/movie/entities/Movie.java
+++ src/main/java/org/tinymediamanager/core/movie/entities/Movie.java
@@ -77,6 +77,7 @@ import org.apache.commons.text.WordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tinymediamanager.core.IMediaInformation;
+import org.tinymediamanager.core.ImageCache;
 import org.tinymediamanager.core.MediaFileHelper;
 import org.tinymediamanager.core.MediaFileType;
 import org.tinymediamanager.core.ScraperMetadataConfig;
@@ -1799,7 +1800,7 @@ public class Movie extends MediaEntity implements IMediaInformation {
     // image files
     List<MediaFile> filesToCache = new ArrayList<>();
     for (MediaFile mf : getMediaFiles()) {
-      if (mf.isGraphic()) {
+      if (mf.isGraphic() && !ImageCache.isImageCached(mf.getFileAsPath())) {
         filesToCache.add(mf);
       }
     }
@@ -1810,7 +1811,11 @@ public class Movie extends MediaEntity implements IMediaInformation {
     // Better get a listing of existent actor images directly!
     if (MovieModuleManager.getInstance().getSettings().isWriteActorImages() && !isMultiMovieDir()) {
       // and only for normal movies - MMD should not have .actors folder!
-      filesToCache.addAll(listActorFiles());
+      for (MediaFile mf : listActorFiles()) {
+        if (mf.isGraphic() && !ImageCache.isImageCached(mf.getFileAsPath())) {
+          filesToCache.add(mf);
+        }
+      }
     } // check against actors and trigger a download? - NO, only via scrape/missingImagesTask
 
     return filesToCache;
