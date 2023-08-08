b635abd735de93f34c9eb6025f56bc62d8d7e101
Myron Boyle <myron0815@gmx.net>
2023-08-08 12:25:35 +0200

    ✨ [imdb] fix episode listing for new style

---
+++ src/main/java/org/tinymediamanager/scraper/imdb/entities/ImdbEpisodeListEpisodes.java
@@ -0,0 +1,29 @@
+package org.tinymediamanager.scraper.imdb.entities;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ImdbEpisodeListEpisodes {
+
+  public int                          total                = 0;
+  public List<ImdbEpisodeListEpisode> items                = null;
+
+  @JsonIgnore
+  private Map<String, Object>         additionalProperties = new HashMap<>();
+
+  @JsonAnySetter
+  public void setAdditionalProperty(String name, Object value) {
+    this.additionalProperties.put(name, value);
+  }
+
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+  }
+}
