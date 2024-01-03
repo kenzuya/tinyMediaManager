c5dfd5d2397cbcd68bf2a7a55614d4f18cb47f37
Manuel Laggner <manuel.laggner@gmail.com>
2024-01-03 17:15:56 +0100

    🐛 enhance searching of duplicates
    #2411

--- src/main/java/org/tinymediamanager/core/movie/MovieList.java
+++ src/main/java/org/tinymediamanager/core/movie/MovieList.java
@@ -1386,7 +1386,7 @@ public final class MovieList extends AbstractModelObject {
       Map<String, Object> ids = movie.getIds();
       for (var entry : ids.entrySet()) {
         // ignore collection "IDs" (tmdbcol is from Ember)
-        if (MediaMetadata.TMDB_SET.equalsIgnoreCase(entry.getKey()) || "tmdbcol".equalsIgnoreCase(entry.getKey())) {
+        if (MediaMetadata.TMDB_SET.equalsIgnoreCase(entry.getKey()) || entry.getKey().toLowerCase(Locale.US).startsWith("tmdbcol")) {
           continue;
         }
 
