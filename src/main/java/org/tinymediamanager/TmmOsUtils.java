766990ff58839c2e5682a5fbbcadc8f58adab92d
Manuel Laggner <manuel.laggner@gmail.com>
2024-01-12 13:55:53 +0100

    🐛 fixed loading native libs in aarch64/amd64 Docker container
    #2434

--- src/main/java/org/tinymediamanager/TmmOsUtils.java
+++ src/main/java/org/tinymediamanager/TmmOsUtils.java
@@ -46,7 +46,7 @@ public class TmmOsUtils {
   public static final String  DESKTOP_FILE = "tinyMediaManager.desktop";
 
   private TmmOsUtils() {
-    // hide public constructor for utility classes
+    throw new IllegalAccessError();
   }
 
   /**
@@ -199,7 +199,7 @@ public class TmmOsUtils {
     }
     // linux
     else if (SystemUtils.IS_OS_LINUX) {
-      if (System.getProperty("os.arch").contains("arm")) {
+      if (System.getProperty("os.arch").contains("arm") || System.getProperty("os.arch").contains("aarch")) {
         nativepath += "arm";
       }
       else {
