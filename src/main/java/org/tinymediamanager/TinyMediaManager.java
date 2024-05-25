23fd720f0d3bbd1169f783d1a6ff5a53669173ad
Manuel Laggner <manuel.laggner@gmail.com>
2024-05-25 09:23:32 +0200

    ⬆️ license update

--- src/main/java/org/tinymediamanager/TinyMediaManager.java
+++ src/main/java/org/tinymediamanager/TinyMediaManager.java
@@ -548,7 +548,7 @@ public final class TinyMediaManager {
     Thread.setDefaultUncaughtExceptionHandler(new Log4jBackstop());
 
     try {
-      License.getInstance().init2118();
+      License.getInstance().init2119();
     }
     catch (Exception e) {
       LOGGER.error("Could not initialize license module!");
